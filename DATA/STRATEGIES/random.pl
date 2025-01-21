4b28a9187b24594fe8e7d8eec17895ea9957e1a3
rh <aga.mensfelt@gmail.com>
2025-01-21 21:11:21 +0000

    Random strategy and Claude interface improved.

--- DATA/STRATEGIES/random.pl
+++ DATA/STRATEGIES/random.pl
@@ -1,6 +1,3 @@
-% Random strategy
-select(_P, _O, S, M) :-
-    % Find all possible moves
-    findall(move(P, Action), possible(move(P, Action), S), Moves),
-    % Randomly select one move from the list
-    random_member(move(P, M), Moves).
+select(P, _O, S, M) :-
+    setof(Mi, possible(move(P, Mi), S), Moves),
+    random_member(M, Moves).
